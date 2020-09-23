ab5ddb59b4d09779b77bc500f4ea16aa3cdce935
trycrmr <trycrmr@gmail.com>
2020-09-23 15:31:57 -0400

    Inits redux

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,18 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+// import { pleaseToggleBool } from "./appSlice";
+import { Counter } from "./Counter.jsx";
 
 const App = (props) => {
+  // console.info(pleaseToggleBool);
+  // const dispatch = useDispatch();
+  // const currentBool = useSelector((state) => state.app.bool);
+  // console.info(currentBool);
   return (
     <div>
       <p>wee bloop blop woop</p>
+      <Counter />
     </div>
   );
 };
